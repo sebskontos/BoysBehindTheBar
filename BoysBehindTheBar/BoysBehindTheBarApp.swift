25609c726a9e390d776c1a452fd6cb9d76639efa
Sebastian Skontos <skontossebastian@gmail.com>
2025-01-26 15:30:16 +1100

    Initial Commit

---
+++ BoysBehindTheBar/BoysBehindTheBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoysBehindTheBarApp.swift
+//  BoysBehindTheBar
+//
+//  Created by Sebastian Skontos on 26/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct BoysBehindTheBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
